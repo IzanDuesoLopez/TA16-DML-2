94e91098c23b74f0b5a846cd1d2114293fba41cd
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-07-26 10:54:32 +0200

    Update Ejercicio1.sql

--- TA16 DML 2/Ejercicio1.sql
+++ TA16 DML 2/Ejercicio1.sql
@@ -1,5 +1,22 @@
 use actividades;
 
+# 1.1
+select NOMBRE from articulos;
+
+# 1.2
+select NOMBRE, PRECIO from articulos;
+
+# 1.3
+select NOMBRE, PRECIO FROM articulos
+where precio >= 200;
+
+# 1.4
+select NOMBRE, PRECIO FROM articulos
+where PRECIO BETWEEN 60 AND 120;
+
+# 1.5
+select NOMBRE, PRECIO * 166.386 FROM articulos
+
 #1.6
 select avg(precio) from articulos;
 
