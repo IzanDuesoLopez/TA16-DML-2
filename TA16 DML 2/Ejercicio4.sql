6fbd61650f9dfa280feb7284bfe4004491bd6187
IzanDuesoLopez <108723551+IzanDuesoLopez@users.noreply.github.com>
2022-07-26 13:29:07 +0200

    Update Ejercicio4.sql

--- TA16 DML 2/Ejercicio4.sql
+++ TA16 DML 2/Ejercicio4.sql
@@ -9,3 +9,19 @@ select * from peliculas where CALIFICACIONEDAD is null;
 
 # 4.4
 select * from salas where pelicula is null;
+
+# 4.5
+select *
+from (salas
+inner join peliculas 
+on salas.PELICULA = peliculas.CODIGO)
+group by salas.CODIGO;
+
+#4.6
+
+
+
+# 4.7
+select distinct p.NOMBRE, s.NOMBRE
+from peliculas AS p, salas AS s
+where s.PELICULA IS NULL;
