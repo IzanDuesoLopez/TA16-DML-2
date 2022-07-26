82345f3a377dfcc0aa0afe08b581e52da381d199
DanielFloria <59457076+DanielFloria@users.noreply.github.com>
2022-07-26 10:51:34 +0200

    Update Ejercicio1.sql

--- TA16 DML 2/Ejercicio1.sql
+++ TA16 DML 2/Ejercicio1.sql
@@ -1,5 +1,41 @@
 use actividades;
 
+#1.6
+select avg(precio) from articulos;
+
+#1.7
+select avg(precio) from articulos where fabricante=2;
+
+#1.8
+select count(codigo) from articulos where precio>=180;
+
+#1.9
+select nombre, precio from articulos
+where precio>=180
+order by precio desc;
+
+select nombre, precio from articulos
+where precio>=180
+order by nombre;
+
+#1.10
+select * from articulos 
+inner join fabricantes 
+on articulos.fabricante=fabricantes.codigo;
+
+#1.11
+select articulos.nombre, articulos.precio, fabricantes.nombre as nombre_fabricante
+from articulos
+inner join fabricantes
+on fabricantes.codigo=articulos.fabricante;
+
+#1.12
+select fabricantes.codigo as codigo_fabricante, avg(articulos.precio) as media_precio
+from articulos
+inner join fabricantes
+on fabricantes.codigo=articulos.fabricante
+group by codigo_fabricante;
+
 #1.13
 select avg(p.PRECIO), n.NOMBRE 
 from articulos AS p, fabricantes AS n
